c3ef6050a6d98a7945c1cc3f830a9b112ae5403a
Isaac <isaaccubas@gmail.com>
2021-04-08 14:50:57 +0200

    Creo la home

---
+++ tests/Feature/Http/Controllers/PageControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Tests\TestCase;
+use App\Models\Repository;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PageControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_home()
+    {
+        $repository = Repository::factory()->create();
+
+        $this
+            ->get('/')
+            ->assertStatus(200)
+            ->assertSee($repository->url);
+    }
+}
